7b68e584e3ef71ab96b2d916a7aa2f638b2fe263
Andriy Taran <andreitdeveloper@gmail.com>
2019-05-01 09:11:58 -0700

    updated UI for add new athlete info page and welcome page

--- client/updatedDesign/constants.js
+++ client/updatedDesign/constants.js
@@ -7,28 +7,23 @@ const attributesNamesMatches = {
 };
 
 const positions = [
-  {title: 'Quarterback', value: 'QB'},
   {title: 'Quarterback Pro Style', value: 'QBPRO'},
   {title: 'Quarterback Dual Style', value: 'QBDUAL'},
   {title: 'Running Back', value: 'RB'},
   {title: 'Wide Receiver', value: 'WR'},
-  {title: 'Offensive Line', value: 'OL'},
-  {title: 'Defensive Line', value: 'DL'},
   {title: 'Tight End', value: 'TE'},
-  {title: 'Linebacker', value: 'LB'},
-  {title: 'Corner Back', value: 'CB'},
-  {title: 'Kicker', value: 'K'},
+  {title: 'Offensive Tackle', value: 'OT'},
+  {title: 'Offensive Guard', value: 'OG'},
   {title: 'Defensive End', value: 'DE'},
   {title: 'Defensive Tackle', value: 'DT'},
-  {title: 'Offensive Guard', value: 'OG'},
-  {title: 'Offensive Tackler', value: 'OT'},
-  {title: 'Defensive Back', value: 'DB'},
-  {title: 'Punter', value: 'P'},
-  {title: 'Safety', value: 'S'},
-  {title: 'Long Snapper', value: 'LS'},
+  {title: 'Outside Linebacker', value: 'OLB'},
   {title: 'Inside Linebacker', value: 'ILB'},
   {title: 'Middle Linebacker', value: 'MLB'},
-  {title: 'Outside Linebacker', value: 'OLB'},
+  {title: 'Defensive Back', value: 'DB'},
+  {title: 'Safety', value: 'S'},
+  {title: 'Punter', value: 'P'},
+  {title: 'Kicker', value: 'K'},
+  {title: 'Long Snapper', value: 'LS'},
 ]
 
 const getPositionValueOnSelect = (value) => {
